eb39857be9335ff144e43851c1b368c20ac99a9a
Serghei Iakovlev <sadhooklay@gmail.com>
2015-10-24 23:02:35 +0300

    Added forms

---
+++ app/forms/ProductTypesForm.php
@@ -0,0 +1,31 @@
+<?php
+
+use Phalcon\Forms\Form;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\Hidden;
+use Phalcon\Validation\Validator\PresenceOf;
+
+class ProductTypesForm extends Form
+{
+    /**
+     * Initialize the products form
+     *
+     * @param mixed $entity
+     * @param array $options
+     */
+    public function initialize($entity = null, array $options = null)
+    {
+        if (!isset($options['edit'])) {
+            $element = new Text("id");
+            $this->add($element->setLabel("Id"));
+        } else {
+            $this->add(new Hidden("id"));
+        }
+
+        $name = new Text("name");
+        $name->setLabel("Name");
+        $name->setFilters(['striptags', 'string']);
+        $name->addValidators([new PresenceOf(['message' => 'Name is required'])]);
+        $this->add($name);
+    }
+}
