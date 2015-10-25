c9452dd237d27fb295f7681e96dd858912bd11e7
Serghei Iakovlev <sadhooklay@gmail.com>
2015-10-25 03:46:53 +0200

    Added initial RoutesCest

---
+++ tests/functional/RoutesCest.php
@@ -0,0 +1,20 @@
+<?php
+
+class RoutesCest
+{
+    public function openPageByRoute(FunctionalTester $I)
+    {
+        $I->amOnRoute('front.contact');
+        $I->see('Contact Us');
+        $I->see('Send us a message and let us know how we can help.');
+        $I->seeCurrentUrlEquals('/contact-us');
+    }
+
+    public function routesWithTrailingSlashes(FunctionalTester $I)
+    {
+        $I->amOnPage('/contact////////');
+        $I->see('Contact Us');
+        $I->see('Send us a message and let us know how we can help.');
+        $I->seeInCurrentUrl('/contact');
+    }
+}
