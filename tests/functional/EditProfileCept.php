d24bb7ad3e19ff034d2e383354371bc41ff8ec73
Serghei Iakovlev <sadhooklay@gmail.com>
2015-10-27 02:08:10 +0200

    Added EditProfileCept

---
+++ tests/functional/EditProfileCept.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @var \Codeception\Scenario $scenario
+ */
+
+$I = new Step\Functional\UserSteps($scenario);
+
+$I->wantTo('edit a profile');
+
+$userId = $I->amRegularUser();
+$I->amOnPage('/login');
+
+$I->fillField('email',    'demo@phalconphp.com');
+$I->fillField('password', 'phalcon');
+$I->click('Login');
+
+$I->amOnPage('/session/start');
+
+$I->click('Profile');
+
+$I->dontSeeInFormFields('#profileForm', [
+    'name' => 'John Doe'
+]);
+
+$I->fillField('name', 'John Doe');
+$I->click('Update');
+
+$I->see('Your profile information was updated successfully');
+
+$I->click('Log Out');
+$I->see('Goodbye John Doe!');
+
